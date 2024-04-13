a9130b2ec7d14bd3e8783fdd1d753357f068ec26
JuanDavidPerdomo <105566368+JuanDavidPerdomo@users.noreply.github.com>
2024-04-12 23:40:03 -0500

    changing stripe key

--- next-checkout/constants.ts
+++ next-checkout/constants.ts
@@ -1,4 +1,4 @@
 export default {
     endpoint: 'http://localhost:8000/api/checkout',
-    stripe_key: 'pk_test_51O14MdK9Gfq6HVtwFDc2L4PO16kULV5OVed7yO8e464Aj0GYplJdv1t0ouOgOaUnPcq724XC4iUilkyZhPbWsozT008NnZ8zBT'
+    stripe_key: 'pk_test_51P4DxCRtcF23bicUbTWbcxdMFaoqHadjI0nfzoS6cnx2WEB8SdBMpa0sMMJERr4RMehfK2apaqr38ayMxKtNJJiE00hXyxhNzE'
 }
